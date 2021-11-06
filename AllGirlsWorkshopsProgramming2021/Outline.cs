c025eee5e14db0971a45e6321fae965f1f233ea1
dorsamousavi <dorsa.m1356@gmail.com>
2021-11-06 11:14:16 -0700

    Initial check-in of module AllGirlsWorkshopsProgramming2021

---
+++ AllGirlsWorkshopsProgramming2021/Outline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/*
+ * functions:
+ * Console.WriteLine("message")
+ *      prints message to terminal
+ * 
+ */
+
+namespace AllGirlsWorkshopsProgramming20211
+{
+    class ChatBot
+    {
+        static void Main(string[] agrs)
+        {
+            //print out an inital question
+            //assign the user's input to a variable 
+            //use a chain of if and else if statments to analyze the user's response
+
+        }
+    }
+}
---
+++ AllGirlsWorkshopsProgramming2021/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AllGirlsWorkshopsProgramming2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How are you?");
+
+            string answer = Console.ReadLine();
+            if(answer == "good" || answer == "amazing")
+            {
+                Console.WriteLine("YAY!");
+            }
+            else if(answer == "okay")
+            {
+                Console.WriteLine("That's okay");
+            }
+            else if(answer == "bad")
+            {
+                Console.WriteLine("I hope you feel better");
+            }
+            else
+            {
+                Console.WriteLine("I didn't catch that.");
+            }
+        }
+    }
+}
